b7f85c42cf83959891e44a4dd15835a515dddcf2
Tanawat Tassana <amp.tanawat@gmail.com>
2017-09-19 19:30:48 +0700

    Set HTML plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 
 const src = path.resolve(__dirname, 'src')
 const dist = path.resolve(__dirname, 'dist')
 
 module.exports = {
   entry: path.resolve(src, 'index.js'),
+  plugins: [
+    new HtmlPlugin({
+      title: 'Soccerlar',
+      template: path.resolve(src, 'index.html')
+    })
+  ],
   output: {
     filename: 'app.js',
     path: dist
