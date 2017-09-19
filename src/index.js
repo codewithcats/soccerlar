e137b152800ff76d908b72cda6fa8e47aef534ef
Tanawat Tassana <amp.tanawat@gmail.com>
2017-09-19 20:50:18 +0700

    Create leagueListController

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import angular from 'angular'
 
 const soccerlarModule = angular.module('soccerlar', [])
+
+soccerlarModule.controller('leagueListController', [
+  '$http',
+  function($http) {
+    console.debug('[leagueListController] init')
+  }
+])
