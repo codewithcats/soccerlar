8df8dfc8fbddd103168c6c58bd411b53820d0d34
Tanawat Tassana <amp.tanawat@gmail.com>
2017-09-20 04:42:44 +0700

    Display league title

--- src/index.js
+++ src/index.js
@@ -32,11 +32,15 @@ soccerlarModule.config([
     const leagueDetail = {
       name: 'leagueDetail',
       url: '/league/{leagueSlug}',
+      controller: 'leagueDetailController',
       template: `
       <section name="league-detail">
         <h1 class="title is-1">
-          League: {{league.name}}
+          {{league.name}}
         </h1>
+        <h4 class="subtitle is-4">
+          {{league.nation}}
+        </h4>
       </section>
       `
     }
@@ -51,7 +55,6 @@ soccerlarModule.controller('leagueListController', [
   '$scope',
   '$state',
   function($http, $scope, $state) {
-    console.debug('[leagueListController] started')
     $http({
       url: 'https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues',
       headers: {
@@ -65,10 +68,27 @@ soccerlarModule.controller('leagueListController', [
     })
 
     $scope.onLeagueClick = (league) => {
-      console.debug('[leagueListController] league clicked', league)
       $state.go('leagueDetail', {
         leagueSlug: league.league_slug
       })
     }
+  }
+])
+
+soccerlarModule.controller('leagueDetailController', [
+  '$http',
+  '$scope',
+  '$stateParams',
+  function($http, $scope, $stateParams) {
+    $http({
+      url: `https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/${$stateParams.leagueSlug}`,
+      headers: {
+        'X-Mashape-Key': 'ZqAO9XW13qmshFt97YltTFOOGDjhp1dHh00jsnD8ztb0FcWmpG',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      $scope.league = response.data.data.leagues[0]
+    })
   }
 ])
